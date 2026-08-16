ea1818300951d871f60655dd2c2434b69a9017f2
Davide Sogos <davidesogos@gmail.com>
2026-08-16 03:25:49 +0200

    Initial Commit

---
+++ Gym Traker/Gym_TrakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gym_TrakerApp.swift
+//  Gym Traker
+//
+//  Created by Davide Sogos on 16/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct Gym_TrakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Gym TrakerTests/Gym_TrakerTests.swift
@@ -0,0 +1,19 @@
+//
+//  Gym_TrakerTests.swift
+//  Gym TrakerTests
+//
+//  Created by Davide Sogos on 16/08/2026.
+//
+
+import Testing
+@testable import Gym_Traker
+
+struct Gym_TrakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
